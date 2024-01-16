919f9c9d8922d177b6748b26f49f09c4751baf0e
kelvin <kelvin4stunt@gmail.com>
2024-01-16 23:44:10 +0100

    up

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Contains the number_of_subscribers function
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+
+    :param subreddit: The name of the subreddit.
+    :return: The number of subscribers or 0 if the subreddit is invalid.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            # Extract the number of subscribers
+            subscribers_count = data['data']['subscribers']
+            return subscribers_count
+        elif response.status_code == 404:
+            return 0
+        else:
+            print(f"An error occurred: {e}")
+            return 0
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 0
