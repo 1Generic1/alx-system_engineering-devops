c8eb93ab2faf6052b3021059fc8545f2cde1c6d0
kelvin <kelvin4stunt@gmail.com>
2024-01-17 08:21:45 +0100

    up

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,7 +16,8 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)'}
-
+    if subreddit is None or type(subreddit) is not str:
+        return 0
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
