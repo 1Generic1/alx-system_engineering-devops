4cb6a1932b52504c1653a95775a447f27d630fea
kelvin <kelvin4stunt@gmail.com>
2024-01-19 08:25:13 +0100

    up

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,9 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+# Fix wordpress config file spelling mistake
+exec {'fix-wordpress':
+command => '/bin/sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php',
+notify  => Service['apache2'],
+}
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+service {'apache2':
+ensure => 'running',
 } 
